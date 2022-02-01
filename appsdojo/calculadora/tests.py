a18b1590da18b8932e93025dbc9415250b561f60
wermeson <wermeson.reis@intmed.com.br>
2022-02-01 16:17:44 -0300

    Criação do teste inicial de cadastro de calculadora

--- appsdojo/calculadora/tests.py
+++ appsdojo/calculadora/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
+from appsdojo.calculadora.models import Calculadora
+from appsdojo.calculadora.serializers import calculadora
+
 # Create your tests here.
+class CalculadoraTest(TestCase):
+    def setUp(self):
+        Calculadora.objects.create(nome="Calculadora Teste", descricao="Teste", expressao="peso + 3")
+
+    def test_criacao_calculadora(self):
+        calculadora = Calculadora.objects.get(nome="Calculadora Teste")
+        self.assertEqual()
