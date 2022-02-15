9c315e8e86cb27897e465aba10e4a1dbd74b20e6
juninhoCarlos <carlos.junior@intmed.com.br>
2022-02-15 15:49:19 -0300

    feat(test): Adicionado teste de atualizacao da calculadora e de teste nome duplicado

--- appsdojo/calculadora/tests/tests.py
+++ appsdojo/calculadora/tests/tests.py
@@ -1,13 +1,36 @@
+from appsdojo.calculadora.serializers import calculadora
+from calculadora.models import Calculadora
+from django.db.utils import IntegrityError
 from django.test import TestCase
 
-from calculadora.models import Calculadora
+# Fazer o teste do CRUD da calculadora
+
+# Verificar nome duplicado
+# Fazer teste do CRUD da variável
+# Validação de nome duplicado na mesma calculadora
 
 # Create your tests here.
 class CalculadoraTest(TestCase):
     def setUp(self):
         Calculadora.objects.create(nome="Calculadora Teste", descricao="Teste", expressao="peso + 3")
+        Calculadora.objects.create(nome="Calculadora Teste 2", descricao="Teste", expressao="peso + 3")
 
     def test_criacao_calculadora(self):
         calculadora = Calculadora.objects.filter(nome="Calculadora Teste")
         self.assertEqual(calculadora.exists(), True)
+
+    def test_atualiza_calculadora(self):
+        calculadora = Calculadora.objects.get(nome="Calculadora Teste")
+        calculadora.nome = "Novo nome atualizado"
+        calculadora.save()
+
+        self.assertEqual(Calculadora.objects.filter(nome="Novo nome atualizado").exists(), True)
+        self.assertEqual(Calculadora.objects.filter(nome="Calculadora Teste").exists(), False)
+
+    def test_atualiza_calculadora_nome_duplicado(self):
+        calculadora = Calculadora.objects.get(nome="Calculadora Teste")
+        calculadora.nome = "Calculadora Teste 2"
+        
+        # Espera um erro de integridade
+        self.assertRaises(IntegrityError, calculadora.save)
 
